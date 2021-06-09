d0f88821aac56f4fd906b1f98f64dea395b9fbff
Naeun <nue.an.2@gmail.com>
2021-06-09 17:48:08 +0900

    DB 2일차 추가

---
+++ practice/src/practice/mail/Mail.java
@@ -0,0 +1,15 @@
+package practice.mail;
+
+import java.util.Properties;
+
+public class Mail {
+	final String user = "na12346534@naver.com";
+	final String password = "password";
+	
+	Properties prop = new Properties();
+//	prop.put("mail.smtp.host", "smtp.naver.com");
+//	prop.put("mail.smtp.host", 587);
+	
+
+	
+}
