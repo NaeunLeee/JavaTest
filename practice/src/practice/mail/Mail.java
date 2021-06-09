d8a59f4880a5d5a14c79aa532abdaa21650c4431
NaeunLeee <nue.an.2@gmail.com>
2021-06-09 22:24:04 +0900

    메일 보내기 과제 (미완)

--- practice/src/practice/mail/Mail.java
+++ practice/src/practice/mail/Mail.java
@@ -3,13 +3,33 @@ package practice.mail;
 import java.util.Properties;
 
 public class Mail {
-	final String user = "na12346534@naver.com";
-	final String password = "password";
+	String host = "smtp.naver.com"; 
+	String user = "ktko@naver.com"; 
+	String password = "password";      
 	
-	Properties prop = new Properties();
-//	prop.put("mail.smtp.host", "smtp.naver.com");
-//	prop.put("mail.smtp.host", 587);
+	Properties props = new Properties(); 
+	props.put("mail.smtp.host", host); 
+	props.put("mail.smtp.port", 587); 
+	props.put("mail.smtp.auth", "true"); 
 	
+	Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+		protected PasswordAuthentication getPasswordAuthentication() { 
+			return new PasswordAuthentication(user, password); } }); 
+	
+	try { 
+		MimeMessage message = new MimeMessage(session); 
+		message.setFrom(new InternetAddress(user)); 
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress("ktko@ktko.com")); 
+		message.setSubject("KTKO SMTP TEST1111"); 
+		message.setText("KTKO Success!!");
+		Transport.send(message); 
+		System.out.println("Success Message Send"); 
+		} catch (MessagingException e) { 
+			e.printStackTrace(); }
+
+    });
+
+
 
 	
 }
